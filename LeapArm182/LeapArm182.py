cb5892476c77b1f9c6bbcaa33e8344be0737a4f6
Miguel Oliveira <pablodorneles27@gmail.com>
2025-09-14 22:54:27 -0300

    LeapArm finalizado

--- LeapArm182/LeapArm182.py
+++ LeapArm182/LeapArm182.py
@@ -1,23 +1,93 @@
-import sys
+import leap
 import serial
 import time
-#import Leap
+
+
+# Posições de calibração dos servos
+GARRA_ABERTA = 65
+GARRA_FECHADA = 110
+ROT_ESQ = 180
+ROT_DIR = 0
+ATAQUE_AVANCO = 140
+ATAQUE_RECUO = 60
+ALTURA_CIMA = 140
+ALTURA_BAIXO = 80
 
 # Configura o serial do Arduino
 arduino = serial.Serial("COM4", 9600)
 time.sleep(2)  # Espera o Arduino resetar
 
-while True:
-    # Lê as posições desejadas
-    print("Digite os ângulos (0-180) ou S para sair")
-    try:
-        garra = int(input("Garra: "))
-        rot = int(input("Rotação: "))
-        ataque = int(input("Ataque: "))
-        altura = int(input("Altura: "))
-    except:
-        arduino.close()
-        sys.exit(0)
 
-    pacote = bytes([garra, rot, ataque, altura])
-    arduino.write(bytes(pacote))
+# Limita extensão dos movimentos
+def clamp(value, min_val=0.0, max_val=1.0):
+    return max(min_val, min(max_val, value))
+
+
+class MyListener(leap.Listener):
+    def on_connection_event(self, event):
+        print("Leap Motion Controller connectado!")
+
+    def on_device_event(self, event):
+        try:
+            with event.device.open():
+                info = event.device.get_info()
+        except leap.LeapCannotOpenDeviceError:
+            info = event.device.get_info()
+
+        print(f"Dispositivo encontrado: {info.serial}")
+
+    def on_tracking_event(self, event):
+        if event.tracking_frame_id % 10 == 0:
+            if len(event.hands):
+                hand = event.hands[0]  # Primeira mão da lista (esquerda ou direita)
+
+                palma_da_mao = clamp(hand.grab_strength)  # 0=aberta, 1=fechado
+                garra = int(GARRA_ABERTA + palma_da_mao * (GARRA_FECHADA - GARRA_ABERTA))
+
+                x_norm = clamp((hand.palm.position.x + 250.0) / 500.0)  # -250 a 250
+                rotacao = int(ROT_ESQ - x_norm * (ROT_ESQ - ROT_DIR))
+                
+                z_norm = 1.0 - clamp((hand.palm.position.z + 120.0) / 240.0)  # -120 a 120
+                ataque = int(ATAQUE_RECUO + z_norm * (ATAQUE_AVANCO - ATAQUE_RECUO))
+                
+                y_norm = clamp((hand.palm.position.y - 150.0) / 200.0)  # 150 a 350
+                altura = int(ALTURA_BAIXO + y_norm * (ALTURA_CIMA - ALTURA_BAIXO))
+                
+                # Imprime os valores normalizados
+                print(
+                    f"Rotação: {x_norm:.3f} | Ataque: {z_norm:.3f} | Altura: {y_norm:.3f} | Palma: {palma_da_mao:.3f})"
+                )
+                
+                pacote = bytes([garra, rotacao, ataque, altura])
+                arduino.write(bytes(pacote))
+
+            else:
+                # Nenhuma mão posicionada, coloca no centro do movimento em todos os eixos
+                pacote = bytes([
+                    int(GARRA_ABERTA + (GARRA_FECHADA - GARRA_ABERTA) / 2),
+                    int(ROT_DIR + (ROT_ESQ - ROT_DIR) / 2),
+                    int(ATAQUE_RECUO + (ATAQUE_AVANCO - ATAQUE_RECUO) / 2),
+                    int(ALTURA_BAIXO + (ALTURA_CIMA - ALTURA_BAIXO) / 2)
+                ])
+                arduino.write(bytes(pacote))
+
+
+def main():
+    my_listener = MyListener()
+
+    connection = leap.Connection()
+    connection.add_listener(my_listener)
+
+    running = True
+
+    with connection.open():
+        connection.set_tracking_mode(leap.TrackingMode.Desktop)
+        while running:
+            time.sleep(1)
+
+    arduino.close()
+
+
+if __name__ == "__main__":
+    main()
+
--- README.md
+++ README.md
@@ -12,3 +12,25 @@ Trabalho de Ciências - Semáforo com Motorzinho
 - Instalar a biblioteca `pyserial` usando o comando `pip install pyserial`.
 - Para calibrar, entre na pasta `LeapArm182` e execute o comando `python Calibracao.py`.
 - Antes de executar os scripts em Python você precisa configurar a porta serial correta no script (ex.: `COM3`).
+
+## Instalação da SDK do Leap Motion
+
+Instalar o software do Leap Motion Controler, versão Gemini (v5.20.0) de [Ultraleap Downloads](https://www.ultraleap.com/downloads/leap-controller/).
+
+Clonar o repositório [leapc-python-bindings](https://github.com/ultraleap/leapc-python-bindings).
+
+Instalar e testar o programa de exemplo
+
+```shell
+cd leapc-python-api
+pip install -r requirements.txt
+pip install -e leapc-python-api
+python examples/tracking_event_example.py
+```
+
+## Para executar o programa
+
+```shell
+cd LeapArm182
+python LeapArm182.py
+```
